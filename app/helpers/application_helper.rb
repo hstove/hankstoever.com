47d69a44e850582cfbb528165667f2f1fdabd5a8
Hank Stoever <hstove@gmail.com>
2013-07-18 11:54:32 -0700

    better post formatting

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -21,4 +21,31 @@ module ApplicationHelper
     markdown = Redcarpet::Markdown.new(Redcarpet::Render::HTML, :autolink => true, :space_after_headers => true)
     markdown.render(text).html_safe
   end
+
+  def to_slug(param=self.slug)
+
+    # strip the string
+    ret = param.strip
+
+    #blow away apostrophes
+    ret.gsub! /['`]/, ""
+
+    # @ --> at, and & --> and
+    ret.gsub! /\s*@\s*/, " at "
+    ret.gsub! /\s*&\s*/, " and "
+
+    # replace all non alphanumeric, periods with dash
+    ret.gsub! /\s*[^A-Za-z0-9\.]\s*/, '-'
+
+    # replace underscore with dash
+    ret.gsub! /[-_]{2,}/, '-'
+
+    # convert double dashes to single
+    ret.gsub! /-+/, "-"
+
+    # strip off leading/trailing dash
+    ret.gsub! /\A[-\.]+|[-\.]+\z/, ""
+
+    ret
+  end
 end
--- app/models/post.rb
+++ app/models/post.rb
@@ -2,4 +2,35 @@ class Post < ActiveRecord::Base
   def html
     Bluecloth.new(body).to_html.html_safe
   end
+
+  def to_param
+    "#{id}-#{slug}"
+  end
+
+  def slug(param=self.title)
+
+    # strip the string
+    ret = param.strip
+
+    #blow away apostrophes
+    ret.gsub! /['`]/, ""
+
+    # @ --> at, and & --> and
+    ret.gsub! /\s*@\s*/, " at "
+    ret.gsub! /\s*&\s*/, " and "
+
+    # replace all non alphanumeric, periods with dash
+    ret.gsub! /\s*[^A-Za-z0-9\.]\s*/, '-'
+
+    # replace underscore with dash
+    ret.gsub! /[-_]{2,}/, '-'
+
+    # convert double dashes to single
+    ret.gsub! /-+/, "-"
+
+    # strip off leading/trailing dash
+    ret.gsub! /\A[-\.]+|[-\.]+\z/, ""
+
+    ret
+  end
 end
