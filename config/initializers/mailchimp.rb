a392adf2d5d498cf862e0d3ea48e28cf14251f8f
Hank Stoever <hstove@gmail.com>
2014-01-08 20:06:24 -0800

    set gibbon defaults before configuration

--- config/initializers/mailchimp.rb
+++ config/initializers/mailchimp.rb
@@ -1,6 +1,6 @@
-Rails.configuration.mailchimp = Gibbon::API.new(ENV['MAILCHIMP_KEY'])
-
 if Rails.env.test?
   Gibbon::Export.api_key = "fake"
   Gibbon::Export.throws_exceptions = false
 end
+
+Rails.configuration.mailchimp = Gibbon::API.new(ENV['MAILCHIMP_KEY'])
