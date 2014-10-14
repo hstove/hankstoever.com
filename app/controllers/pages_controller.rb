9b75447bf84653c9c7e5da17a644dd033c99c11e
hank <hstove@gmail.com>
2014-10-13 23:36:45 -0700

    better book page

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -3,6 +3,8 @@ class PagesController < ApplicationController
   def show
     page = params[:template]
     if page.include? "book"
+      main = "self-taught-software-developers-handbook"
+      return redirect_to page_path(main) unless page == main
       page = "book"
     end
     render page
