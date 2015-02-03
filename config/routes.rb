86ec53facc6781b292fb11ac31fe7307eccbc310
hank <hstove@gmail.com>
2015-02-03 13:55:36 -0800

    root to projects page

--- config/routes.rb
+++ config/routes.rb
@@ -13,7 +13,7 @@ Heynk::Application.routes.draw do
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
-  root 'posts#index'
+  root 'pages#show', template: 'projects'
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
